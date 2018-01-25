ac550bdb8b40851a567678066bd0440d41db9797
Zack Kollar <rallokkcaz@gmail.com>
2018-01-24 23:40:37 -0800

    Stupid me.

--- build/ivy.js
+++ build/ivy.js
@@ -65,14 +65,9 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-
-
-window.onload = function () {
-    console.log('Hello World');
-};
+throw new Error("Module build failed: ReferenceError: Unknown plugin \"transform-object-rest-spread\" specified in \"/home/rombus/Workspace/git/js/ivy/.babelrc\" at 0, attempted to resolve relative to \"/home/rombus/Workspace/git/js/ivy\"\n    at /home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/options/option-manager.js:180:17\n    at Array.map (<anonymous>)\n    at Function.normalisePlugins (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/options/option-manager.js:158:20)\n    at OptionManager.mergeOptions (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/options/option-manager.js:234:36)\n    at OptionManager.init (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/home/rombus/Workspace/git/js/ivy/node_modules/babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/home/rombus/Workspace/git/js/ivy/node_modules/babel-loader/lib/index.js:50:20)\n    at Object.module.exports (/home/rombus/Workspace/git/js/ivy/node_modules/babel-loader/lib/index.js:175:20)");
 
 /***/ })
 /******/ ]);
